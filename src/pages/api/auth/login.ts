5527f5453fdb57436ea18f6e27c9b77655a2c39c
Jordan Minott <jgminott@gmail.com>
2022-10-24 00:35:32 +0300

    Fixed register/login API endpoints

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,13 +1,14 @@
 import prisma from "../_config";
 import { NextApiRequest, NextApiResponse } from "next";
-import bcrypt from "bcrypt";
-import { v4 as uuid } from "uuid";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method == "POST") {
     const { username, password } = req.body;
 
-    if (!username || !password) return res.status(400);
+    if (!username || !password)
+      return res.status(400).send("Username and password required.");
 
     try {
       const user = await prisma.user.findFirst({
@@ -16,13 +17,29 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         },
       });
 
-      if (user?.password) {
-        const match = await bcrypt.compare(password, user.password);
-        //Return cookies here
+      const match = await compare(password, user!.password!);
+      if (match) {
+        //If username & password are correct return a token
+        const token = sign(
+          {
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
+            username: user!.username,
+          },
+          process.env.JSON_SECRET!
+        );
+        return res.send(token);
+      } else {
+        //If usernamae/password is wrong
+        return res.status(400).send("Username or password incorrect.");
       }
     } catch (err) {
+      //Connection errors
       console.log(err);
+      return res.status(500).send("An error has occured.");
     }
+  } else {
+    //If method is not POST
+    return res.status(405).send("Invalid request method.");
   }
 };
 
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,23 +1,35 @@
 import prisma from "../_config";
 import { NextApiRequest, NextApiResponse } from "next";
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method == "POST") {
     const { username, password, email } = req.body;
     if (password.length < 6) return res.status(400).send("Invalid password.");
     bcrypt.hash(password, 10, async (err, hash) => {
-      const user = await prisma.user.create({
-        data: {
-          username: username,
-          password: hash,
-          email: email || null,
-        },
-      });
-      return res.send(`User ${username} created.`);
+      try {
+        const exists = await prisma.user.findMany({
+          where: {
+            username: username,
+          },
+        });
+
+        if (exists) return res.status(400).send("User already exists.");
+
+        const user = await prisma.user.create({
+          data: {
+            username: username,
+            password: hash,
+            email: email || null,
+          },
+        });
+        return res.status(201).send(`User ${username} created.`);
+      } catch {
+        return res.status(500).send("A problem has occured.");
+      }
     });
   } else {
-    return res.status(500);
+    return res.status(405).send("Invalid request method.");
   }
 };
 
