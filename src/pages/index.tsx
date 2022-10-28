f5c163d75d49295747c0e1dba672bc3dd11a0aab
Jordan Minott <jgminott@gmail.com>
2022-10-28 15:45:37 +0300

    Moved login form to its own component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,37 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { Email, Key } from "@mui/icons-material";
-import { Checkbox } from "@mui/material";
-import styles from "../styles/index.module.css";
+import type { GetServerSideProps } from "next";
+import { verifyToken } from "../lib/auth";
 
-const Home: NextPage = () => {
+import { getCookie } from "cookies-next";
+import LoginForm from "../components/LoginForm";
+
+type Props = {};
+
+const Home = () => {
   return (
-    <div className={`flex items-stretch h100`}>
-      <div className="p20 flex-grow">fdsgds</div>
-
-      <div className={`${styles.signinbox} flex-col`}>
-        <h2>Get started!</h2>
-        <div>
-          <form className="flex-col">
-            <div className={styles.textbox}>
-              <Email />
-              <input placeholder="Email" required id="email" type="email" />
-            </div>
-            <div className={styles.textbox}>
-              <Key />
-              <input placeholder="Password" type="password" />
-            </div>
-            <div className="flex-row flex-align flex-justify-end">
-              <p>Stay logged in?</p>
-              <Checkbox color="secondary" sx={{ color: "var(--pink)" }} />
-            </div>
-            <button type="submit" className="button-green p5">
-              Sign Up
-            </button>
-          </form>
-        </div>
+    <div className="flex flex-row-reverse flex-auto">
+      <div className="w-96">
+        <LoginForm />
       </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const token = getCookie("access-token", { req, res });
+  if (!token) return { props: {} };
+  const loggedIn = verifyToken(token?.valueOf().toString()!);
+  if (loggedIn) {
+    return {
+      redirect: {
+        destination: "/posts",
+      },
+      props: {},
+    };
+  } else {
+    return {
+      props: {},
+    };
+  }
 };
 
 export default Home;
