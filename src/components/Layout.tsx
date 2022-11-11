2707ea849b931eb7f4c8f005205ad83b89ae3d02
Jordan Minott <jgminott@gmail.com>
2022-11-11 14:13:04 +0300

    Made style reactive for mobile

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import Meta from './Meta'
+import Meta from "./Meta";
 
 type Props = {
   children?: React.ReactNode;
@@ -7,8 +7,11 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <>
-      <Meta title="oof" description="Anonymously post your wildest thoughts or questions." 
+      <Meta
+        title="oof"
+        description="Anonymously post your wildest thoughts or questions."
       />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <main> {children} </main>
     </>
   );
