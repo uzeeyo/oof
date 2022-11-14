ab2d6ddee4cbc9e118f055aa56339619e62123e1
Jordan Minott <jgminott@gmail.com>
2022-11-14 17:39:28 +0300

    Added endpoint for logging out

---
+++ src/pages/api/auth/logout.ts
@@ -0,0 +1,21 @@
+import { deleteCookie } from "cookies-next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { verifyLogin } from "../../../lib/auth";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const loggedIn = verifyLogin({ req, res });
+    if (loggedIn.err) {
+      return res.status(400).end();
+    }
+
+    deleteCookie("access-token", { req, res });
+
+    return res.send({ user: { loggedOut: true } });
+  } else {
+    return res.status(405).end();
+  }
+}
