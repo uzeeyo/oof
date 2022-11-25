0e5b02f59bcd7fa5ba6193cb6e211a6d8a2a6ca7
Jordan Minott <jgminott@gmail.com>
2022-11-25 19:07:21 +0300

    Minor changes to auth provider

--- src/lib/AuthProvider.tsx
+++ src/lib/AuthProvider.tsx
@@ -28,7 +28,6 @@ export const AuthProvider = ({ children }: Props) => {
 
   useEffect(() => {
     const userId = localStorage.getItem("userId");
-    console.log(userId);
     if (userId) setIsLoggedIn(true);
   }, []);
 
